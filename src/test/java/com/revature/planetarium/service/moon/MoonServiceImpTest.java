301842ac63f1eb8141c9e22d7e31a97011b90d5b
justin951 <justin951@revature.com>
2024-08-22 15:33:09 -0400

    added 'user' to serviceImp and daoImp variables in userServiceTestImp

--- src/test/java/com/revature/planetarium/service/moon/MoonServiceImpTest.java
+++ src/test/java/com/revature/planetarium/service/moon/MoonServiceImpTest.java
@@ -3,13 +3,21 @@ package com.revature.planetarium.service.moon;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.revature.planetarium.repository.moon.MoonDaoImp;
 
 import static org.junit.Assert.*;
 
 public class MoonServiceImpTest {
 
+    private MoonDaoImp moonDaoImp;
+    private MoonServiceImp moonServiceImp;
+
     @Before
     public void setUp() throws Exception {
+        moonDaoImp = Mockito.mock(MoonDaoImp.class);
+        moonServiceImp = new MoonServiceImp<>(moonDaoImp);
     }
 
     @After
