3fd7391b4ca3ad04a949cd70f252ab1db27f4db5
Kevin Toyloy <kevintoyloy34@gmail.com>
2024-08-28 11:57:11 -0400

    planet dao test implementation

--- src/test/java/com/revature/planetarium/repository/planet/PlanetDaoImpTest.java
+++ src/test/java/com/revature/planetarium/repository/planet/PlanetDaoImpTest.java
@@ -1,15 +1,39 @@
 package com.revature.planetarium.repository.planet;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import com.revature.Setup;
+import com.revature.planetarium.entities.Planet;
+import com.revature.planetarium.utility.DatabaseConnector;
+import org.junit.*;
 
-import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
 
 public class PlanetDaoImpTest {
 
+    private Planet createdPlanet;
+    private Planet existingPlanet;
+
+    private PlanetDao dao;
+
+    @BeforeClass
+    public static void testDatabaseSetup() throws SQLException {
+        Setup.getConnection();
+        Setup.resetTestDatabase();
+    }
+
     @Before
     public void setUp() throws Exception {
+        createdPlanet = new Planet();
+        createdPlanet.setPlanetName("TestingPlanet");
+        createdPlanet.setOwnerId(1);
+        existingPlanet = new Planet();
+        existingPlanet.setPlanetName("Earth");
+        existingPlanet.setPlanetId(1);
+        existingPlanet.setOwnerId(1);
+
+        dao = new PlanetDaoImp();
     }
 
     @After
@@ -17,34 +41,99 @@ public class PlanetDaoImpTest {
     }
 
     @Test
-    public void createPlanet() {
+    public void createPlanetPositive() {
+        //createdPlanet.setImageData("src/test/resources/Celestial-Images/planet-1.jpg");
+        Optional<Planet> returnedPlanet = dao.createPlanet(createdPlanet);
+        System.out.println(returnedPlanet.isPresent());
+
+        Assert.assertSame(createdPlanet, returnedPlanet.get());
     }
 
     @Test
-    public void readPlanet() {
+    public void readPlanetByIdPositive() {
+        Assert.assertEquals(dao.readPlanet(1), Optional.of(existingPlanet));
     }
 
     @Test
-    public void testReadPlanet() {
+    public void readPlanetByNamePositive() {
+        Assert.assertEquals(dao.readPlanet("Earth"), Optional.of(existingPlanet));
     }
 
     @Test
-    public void readAllPlanets() {
+    public void readPlanetByIdNegative() {
+        //Assuming there are fewer than 50 planets
+        Assert.assertEquals(dao.readPlanet(50), Optional.empty());
     }
 
     @Test
-    public void readPlanetsByOwner() {
+    public void readPlanetByNameNegative() {
+        Assert.assertEquals(dao.readPlanet("thisPlanetDoesNotExist"), Optional.empty());
     }
 
     @Test
-    public void updatePlanet() {
+    public void readAllPlanetsPositive() {
+        Assert.assertTrue(dao.readAllPlanets().size() > 1);
     }
 
     @Test
-    public void deletePlanet() {
+    public void readAllPlanetsNegative() {
+        deletePlanetsForNegativeReadAllPlanetsTest();
+        Assert.assertEquals(0, dao.readAllPlanets().size());
+    }
+
+    public void deletePlanetsForNegativeReadAllPlanetsTest() {
+        try (Connection connection = DatabaseConnector.getConnection()){
+            String sql = "DELETE FROM planets";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new AssertionError("Could not delete planets");
+        }
     }
 
     @Test
-    public void testDeletePlanet() {
+    public void readPlanetsByOwnerPositive() {
+        //Assumption user 1 only has the database setup planets added
+        Assert.assertEquals(2, dao.readPlanetsByOwner(1).size());
     }
+
+    @Test
+    public void readPlanetsByOwnerNegative() {
+        //Assumption no user with id=3
+        Assert.assertEquals(0, dao.readPlanetsByOwner(3).size());
+    }
+
+    @Test
+    public void updatePlanetPositive() {
+        existingPlanet.setPlanetName("Tatooine");
+        Assert.assertEquals(Optional.of(existingPlanet), dao.updatePlanet(existingPlanet));
+    }
+
+    @Test
+    public void updatePlanetNegative() {
+        createdPlanet.setPlanetName("Tatooine");
+        Assert.assertEquals(Optional.empty(), dao.updatePlanet(createdPlanet));
+    }
+
+    @Test
+    public void deletePlanetByIdPositive() {
+        Assert.assertTrue(dao.deletePlanet(1));
+    }
+
+    @Test
+    public void deletePlanetByNamePositive() {
+        Assert.assertTrue(dao.deletePlanet("Earth"));
+    }
+
+    @Test
+    public void deletePlanetByIdNegative() {
+        Assert.assertFalse(dao.deletePlanet(50));
+    }
+
+    @Test
+    public void deletePlanetByNameNegative() {
+        Assert.assertFalse(dao.deletePlanet("thisPlanetDoesNotExist"));
+    }
+
+
 }
